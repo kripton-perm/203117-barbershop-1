3e8d592010178bdb068462f5b989b286a85d1d93
kripton-perm <mixail1024070@mail.ru>
2017-09-08 14:24:13 +0500

    Прайс-лист и оживление всплывающих окон с помощью JS

---
+++ js/main.js
@@ -0,0 +1,58 @@
+var link = document.querySelector(".login-link");
+var popup = document.querySelector(".modal-login");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector(".login-form");
+var login = popup.querySelector("[name=login]");
+var password = popup.querySelector("[name=password]");
+var storage = localStorage.getItem("login");
+var mapLink = document.querySelector(".contacts-button-map");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+link.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("modal-show");
+	if (storage) {
+		login.value = storage;
+		password.focus();
+	} else {
+		login.focus();
+	}
+});
+
+mapLink.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	mapPopup.classList.add("modal-map-show");
+});
+
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error");
+});
+
+mapClose.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	mapPopup.classList.remove("modal-map-show");
+});
+
+form.addEventListener("submit", function (evt) {
+	if (!login.value || !password.value) {
+		evt.preventDefault();
+		popup.classList.add("modal-error");
+	} else {
+		localStorage.setItem("login", login.value);
+	}
+});
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		if (popup.classList.contains("modal-show")) {
+			popup.classList.remove("modal-show");
+			popup.classList.remove("modal-error");
+		}
+		if (mapPopup.classList.contains("modal-map-show")) {
+			mapPopup.classList.remove("modal-map-show");
+		}
+	}
+});
